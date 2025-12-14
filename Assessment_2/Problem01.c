e39adaf9464d28af2409445e707db1670c00567a
raghul40 <raraghul2005@gmail.com>
2025-12-14 09:17:46 +0530

    C Problems

---
+++ Assessment_2/Problem01.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num,result;
+    printf("Enter an number:");
+    scanf("%d",&num);
+    result=(num==50)?1:0;
+    printf("Result:%d",result);
+    return 0;
+}
---
+++ Assessment_2/Problem02.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int num,result;
+    printf("Enter an number:");
+    scanf("%d",&num);
+    result=(num!=50)?1:0;
+    printf("Result:%d",result);
+}
