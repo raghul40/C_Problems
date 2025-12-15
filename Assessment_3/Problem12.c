4ad5be8e146b6a839ba5f4d011de06bbefe8caf0
raghul40 <raraghul2005@gmail.com>
2025-12-15 19:21:37 +0530

    C Problems

---
+++ Assessment_3/Problem12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num,digit0,digit1;
+    printf("Enter an two-digit number : ");
+    scanf("%d",&num);
+    digit0=num%10;
+    digit1=num/10;
+    if(digit0<=digit1){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
