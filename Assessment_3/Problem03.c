096d9fa1e267385e2d86c7d3b457ddfe26613e07
raghul40 <raraghul2005@gmail.com>
2025-12-15 19:09:51 +0530

    C Problems

---
+++ Assessment_3/Problem03.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter an number:");
+    scanf("%d",&num);
+    if(num<50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
