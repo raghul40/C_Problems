26f270516c15c9e281081429cb12e13b98a48840
RAGHUL R A <raraghul2005@gmail.com>
2025-12-29 11:45:11 +0530

    C Problems
    Refactor main function to use variable 'n' and update loop logic.

--- Assessment_04/Problem08.c
+++ Assessment_04/Problem08.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-int main(){
-    int i,d0,d1,sum;
-    i =10;
-    Loop:
-    d0=i%10;
-    d1=i/10;
-    sum=d0+d1;
-    if(sum==6)
-      if(i%2==0)
-        printf("%d\n",i);
-    i++;
-    
-    if(i<=98)
+int main()
+{
+    int n,d0,d1,sum;
+    n = 11;
+    Loop:if(n > 10)
+    {
+        d0 = n % 10;
+        d1 = n / 10;
+        sum = d0 + d1;
+        if(sum == 6 && n % 2 == 0)
+        {
+            printf("%d\n",n);
+        }
+        n++;
         goto Loop;
-    return 0;
+    }
 }
