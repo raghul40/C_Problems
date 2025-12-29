10e5bd89a013a9164ed76fe730674e9f6540046b
RAGHUL R A <raraghul2005@gmail.com>
2025-12-29 18:49:36 +0530

    C Problems

--- Assessment_05/Problem11.c
+++ Assessment_05/Problem11.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-int main()
-{
-    int i,count;
-    count=0;
-    printf("Enter an number: ");
-    scanf("%d",&i);
-    for(;i!=0;i/=10)
+int main(){
+    int n,count;
+    count = 0;
+    printf("Enter an number : " );
+    scanf("%d",&n);
+    for(;n>0;n/=10)
     {
-        count++;
+        count = count + 1;
     }
     printf("%d",count);
 }
