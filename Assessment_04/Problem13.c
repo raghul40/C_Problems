049b196d765b8e66251f434389780ca76ee82892
RAGHUL R A <raraghul2005@gmail.com>
2025-12-29 12:09:20 +0530

    C Problems

--- Assessment_04/Problem13.c
+++ Assessment_04/Problem13.c
@@ -1,22 +1,16 @@
 #include<stdio.h>
-int main(){
-    int a,rev,digit;
-    rev=0;
-    printf("Enter a number: ");
-    scanf("%d",&a);
-    if(a == 0)
-    {
-        rev = 0;
-        goto Print;
-    }
-    Loop:
-        if(a != 0)
-        {
-            digit = a % 10;
-            rev = rev * 10 + digit;
-            a = a / 10;
-            goto Loop; 
-        }
-    Print:
-    printf("%d",rev);
+int main()
+{
+   int n,rev,digit;
+   printf("Enter an number : ");
+   scanf("%d",&n);
+   rev = 0;
+   Loop:if(n >0)
+   {
+       digit = n % 10;
+       rev = (rev*10) + digit;
+       n = n / 10;
+       goto Loop;
+   }
+   printf("%d",rev);
 }
