47e71188e9e325ccb738b15c1ec8d101ab632ebb
RAGHUL R A <raraghul2005@gmail.com>
2025-12-29 11:32:48 +0530

    C Problems
    Refactor main function to use a loop instead of goto for printing odd numbers.

--- Assessment_04/Problem05.c
+++ Assessment_04/Problem05.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
-int main(){
-    int i;
-    i=1;
-    Loop:
-    if(i%2!=0)
-        printf("%d\n",i);
-    i++;
-
-    if(i<=9)
-        goto Loop;
-    return 0;
+int main()
+{
+    int n;
+    n=1;
+    Loop:if(n<10)
+    {
+      if(n % 2 != 0)
+      {
+          printf("%d\n",n);
+      }
+      n++;
+      goto Loop;
+    }
 }
