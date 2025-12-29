432fbcebbede4921eb39a33faaebc35487b28967
RAGHUL R A <raraghul2005@gmail.com>
2025-12-29 19:08:49 +0530

    C Problems
    Refactor main function to improve variable names and logic for reversing a number.

--- Assessment_05/Problem14.c
+++ Assessment_05/Problem14.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
-int main(){
-    int num,a,temp,first,middle,last,result;
-    a=1;
-    result=0;
+int main()
+{
+    int n,temp,first,last,mid,d,res;
+    d=1;
     printf("Enter an number : ");
-    scanf("%d",&num);
-    temp = num;
-    last=num%10;
-    for(;temp>=10;temp/=10)
+    scanf("%d",&n);
+    temp = n ;
+    last = n % 10;
+    for(;n>10;n/=10)
     {
-        a = a * 10 ;
+        d = d * 10;
     }
-    first = temp ;
-    middle = (num%a)/10;
-    result = (last*a)+(middle*10)+first;
-    printf("%d",result);
-    
+    first = n ;
+    last = last * d;
+    mid = (temp % d)/10;
+    res = last + (mid * 10) + first;
+    printf("%d",res);
 }
