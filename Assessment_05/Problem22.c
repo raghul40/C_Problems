76ae7c208c6ddac8b0113fc536e05ab8777614da
RAGHUL R A <raraghul2005@gmail.com>
2025-12-30 12:20:02 +0530

    C Problems

--- Assessment_05/Problem22.c
+++ Assessment_05/Problem22.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
-int main(){
-    int num,d,count=0;
-    printf("Enter an number : ");
+int main()
+{
+    int num,digits,count;
+    count = 0;
+    printf("Enter an number : " );
     scanf("%d",&num);
-    for(;num>=10;num/=10)
+    for(;num>10;num/=10)
     {
-        d = num % 100 ;
-        if (d % 2 != 0)
+        digits = num % 100;
+        if(digits % 2 != 0)
         {
-            count = count + 1;
+            count = count + 1 ;
         }
-    }
+    }  
     printf("%d",count);
 }
