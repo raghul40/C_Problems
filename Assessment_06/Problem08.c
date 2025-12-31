c8105ba857f313755471daff238e6c5cfeeb510f
raghul40 <raraghul2005@gmail.com>
2025-12-31 11:53:20 +0530

    C Problems

---
+++ Assessment_06/Problem08.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,q,r,sum;
+    i = 10;
+    sum = 0 ;
+    while(i<99)
+    {
+      q = i / 10;
+      r = i % 10;
+      sum = q + r;
+      if((i % 2 == 0) && (sum == 6))
+      {
+          printf("%d\n",i);
+      }
+      i = i + 2;
+    }
+}
