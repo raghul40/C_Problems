e117c00b8e4fbb3d52c8be8530a78c589f1792b9
raghul40 <raraghul2005@gmail.com>
2025-12-31 11:11:21 +0530

    C Problems

--- Assessment_06/Problem01.c
+++ Assessment_06/Problem01.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-int main(){
-    int i,result;
+int main()
+{
+    int i;
     i=1;
-    for(i=1;i<=5;i++){
-        result = i ;
-        printf("%d\n",result);
+    while(i<6)
+    {
+        printf("%d\n",i);
+        i = i + 1;
     }
-    
 }
