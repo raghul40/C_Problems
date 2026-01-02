5ad7da0c7f6502bddb987385ffac9e25372faac6
raghul40 <raraghul2005@gmail.com>
2026-01-02 21:59:34 +0530

    C Problems

--- Assessment_07/Problem10.c
+++ Assessment_07/Problem10.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
-void disp_2digit_ones5(int);
+void disp_2digit_odd_sum_tens7(int);
 int main()
 {
     int x;
     scanf("%d",&x);
-    disp_2digit_ones5(x);
+    disp_2digit_odd_sum_tens7(x);
 }
-void disp_2digit_ones5(int x)
+void disp_2digit_odd_sum_tens7(int x)
 {
     int i,digit,sum;
     sum = 0;
     i = x;
     while(i<=99)
     {
-        digit = i % 10;
-        if(digit == 5)
+        digit = i / 10;
+        if((digit == 7) && (i % 2 != 0))
         {
            sum = sum + i;
         }
