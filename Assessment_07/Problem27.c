80222cee98015b50f5c90dd467d8ed365d322abf
raghul40 <raraghul2005@gmail.com>
2026-01-08 12:23:38 +0530

    C Problems

---
+++ Assessment_07/Problem27.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void disp_count_sum14(int);
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    disp_count_sum14(x);
+}
+void disp_count_sum14(int x)
+{
+   int n,i,digit,count,sum;
+    count = 0;
+    i = x;
+    while(i < 100000)
+    {
+        n = i;
+        sum = 0;
+        while(n > 0)
+        {
+            digit = n % 10 ;
+            sum = sum + digit;
+            n = n / 10;
+        }
+        if(sum == 14)
+        {
+            count = count + 1;
+        }
+        i = i + 1;
+    }
+    printf("%d",count);
+}
