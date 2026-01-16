8381a01e608dad90ebf6fa991b6b9037c9abd132
raghul40 <raraghul2005@gmail.com>
2026-01-16 14:05:53 +0530

    C Problems

---
+++ Assessment_09/Problem04.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],i,big;
+    printf("Enter 5 numbers:\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    big = a[0];
+    for(i = 1; i < 5; i++) {
+        if(big<a[i])
+        {
+            big = a[i];
+        }
+    }
+    printf("%d",big);
+}
---
+++ Assessment_09/Problem06.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],i,j,tmp;
+    printf("Enter 5 numbers:\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++){
+        for(j=i+1;j<5;j++){
+            if(a[i]>a[j])
+            {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+    printf("%d\n");
+    for(i=0;i<5;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+}
