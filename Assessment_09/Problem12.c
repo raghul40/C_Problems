f9ff346895ade4eb75fc0a6d641e98f1b4eb9e55
raghul40 <raraghul2005@gmail.com>
2026-01-18 12:33:21 +0530

    C Problems

---
+++ Assessment_09/Problem12.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a[100];
+    int num,i,count,sum;
+    i=0;
+    sum=0;
+    count=0;
+    printf("Enter numbers : \n");
+    while(1)
+    {
+        if(num == 0)
+        {
+            break;
+        }
+        a[i] = num;
+        sum = sum + num;
+        count = count + 1;
+        i = i + 1;
+    }
+    printf("\nTotal Numbers Entered : %d",count);
+    printf("\nSum:%d",sum);
+}
